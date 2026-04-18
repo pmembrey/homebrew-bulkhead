71bdd6875e97e80e031d57c1b9b41692ee856c20
Peter Membrey <peter@membrey.hk>
2026-04-19 04:02:06 +0800

    feat: add bulkhead Homebrew formula

---
+++ Formula/bulkhead.rb
@@ -0,0 +1,25 @@
+class Bulkhead < Formula
+  desc "Hardened devcontainer CLI for local agent work"
+  homepage "https://github.com/pmembrey/bulkhead"
+  url "https://github.com/pmembrey/bulkhead/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "971515ce2b15b200cc8fe2852225588b4aa5e240b713abad240edb02efb1396a"
+  license "MIT OR Apache-2.0"
+  head "https://github.com/pmembrey/bulkhead.git", branch: "main"
+
+  depends_on "rust" => :build
+  depends_on "devcontainer"
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    workspace = testpath/"workspace"
+    workspace.mkpath
+
+    system bin/"bulkhead", "template", workspace, "--yes"
+
+    assert_path_exists workspace/"bulkhead.toml"
+    assert_path_exists workspace/".devcontainer/devcontainer.json"
+  end
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# homebrew-bulkhead
+
+Homebrew tap for [bulkhead](https://github.com/pmembrey/bulkhead).
+
+## Install
+
+```bash
+brew tap pmembrey/bulkhead
+brew install bulkhead
+```
